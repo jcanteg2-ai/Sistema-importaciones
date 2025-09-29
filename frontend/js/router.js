d99df66089fa7eac21fa297f4684b03184941afb
Bryan Esquipulas <bryanesquii1@gmail.com>
2025-09-28 22:01:18 -0600

    Se finaliza la vista de login de usuario

--- frontend/js/router.js
+++ frontend/js/router.js
@@ -1,6 +1,11 @@
-const btn = document.getElementById("btn");
 const container = document.querySelector(".container");
+const btnSingIn = document.getElementById("btn-sign-in");
+const btnSingUp = document.getElementById("btn-sign-up");
 
-btn.addEventListener("click", () => {
-    container.classList.toggle("toggle");
+btnSingIn.addEventListener("click", ()=>{
+container.classList.remove("toggle");
 });
+btnSingUp.addEventListener("click", ()=>{
+container.classList.add("toggle");
+});
+
